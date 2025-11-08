c563703faa005668037f247c45587f13ce753176
ahmedalshagga <ahmedalshagga88@gmail.com>
2025-11-08 16:12:29 +0000

    Cleaned 2 Columns

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -4,7 +4,30 @@ getwd()
 setwd("~/Desktop/DBDM COURSEWORK DATA/processed_data/")
 
 data <-read.csv("merged_output.csv")
+
+
+#Checking unique values in each field
+
 for (field in colnames(data)) {
   count <- length(unique(data[ ,field]))
   print(paste("Currently", count, "unique values in", field))
 }
+
+
+
+#Cross referencing with SOP and Cleaning
+print(unique(data[ ,"mouse_life_stage"]))#Contains Typos which have to be cleaned
+data <- data %>% mutate(mouse_life_stage=na_if(str_to_sentence(str_squish(mouse_life_stage)),"na"))
+
+
+print(unique(data[ ,"mouse_strain"])) #Also contains typos, should only have 4:C57BL; B6J; C3H; 129SV
+data %>%
+  group_by(mouse_strain) %>%
+  summarise(count=n(), .groups="drop") #Identifies typos result in decreased C57Bl and 129sv count
+
+data <-data%>%
+  mutate(mouse_strain=case_when(str_detect(mouse_strain,"C5[0-9]BL")~ "C57BL",
+                                str_detect(mouse_strain,"12[0-9]SV")~"129sv",)
+  ) #Fixes Typos
+                                
+
