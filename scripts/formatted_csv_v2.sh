fedc61e96cc642a10bfb909a7cb1742146f1c674
Ahmed Al-Shagga <k25121144@erc-hpc-login1.create.kcl.ac.uk>
2025-11-09 23:31:25 +0000

    Iteriated previous format script to run in parralel and cut time down

--- scripts/formatted_csv_v2.sh
+++ scripts/formatted_csv_v2.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
-#SETUP
-INPUT_DIR="data"
-OUTPUT_DIR="formatted_csvs"
-
-mkdir -p "$OUTPUT_DIR" #Creates fornat.csvs directory if not present
-
-
-printf "Retrieving headers (in parralel)...\n"
-
-CPU_CORES=8  #Selecting amount of CPU Cores to run this task in parralel
-
-HEADER_STRING=$(find $INPUT_DIR "-name ".csv" -print0 | \ 
-xargs -0 -p "$CPU_CORES -n 100 awk-F ',' '$1 != "" {print tolower ($1)}'
-sort -u |tr #!/bin/bash
 
 #Setup
 INPUT_DIR="data"
 OUTPUT_DIR="formatted_csvs"
 
-
 mkdir -p "$OUTPUT_DIR" # Create formatted_csvs directory if not present 
 
-
 printf "Retrieving headers (in parallel)...\n" #Parralel head gathering process
-
 
 CPU_CORES=8 #Manually set to using 8 CPU Cores for parralel tasks
 
@@ -34,7 +17,6 @@ sort -u | tr '\n' ',' | sed 's/,$//')
 
 printf "Headers retrieved! Found %s unique headers.\n" "$(echo "$HEADER_STRING" | tr ',' '\n' | wc -l)"
 
--
 NUM_OF_CSV_FILES=$(ls "$INPUT_DIR"/*.csv | wc -l | xargs)
 printf "Progress: 0/%s" "$NUM_OF_CSV_FILES" #Runs awk once per file 
 
@@ -43,56 +25,44 @@ x=0
 for csv_file in "$INPUT_DIR"/*.csv; do
     x=$((x+1))
     output_file="$OUTPUT_DIR/$(basename "$csv_file")"
-    
+
     # Write the master header string to the new file
     echo "$HEADER_STRING" > "$output_file"
 
-    # --- This is the new, fast awk command ---
-
-
-    # 2. BEGIN block: awk splits this string into its own 'header_array'
-    # 3. Main block: awk reads the file line-by-line, storing values in a 'data' array (e.g., data["param_name"] = "value")
-    # 4. END block: After reading the whole file, awk loops through the 'header_array' (in the correct order)
-    #    and prints the corresponding value from its 'data' array.
-    
-    awk -F',' -v headers="$HEADER_STRING" '  # We run awk ONCE per file.
+    #  This is the new, fast awk command
+     awk -F',' -v headers="$HEADER_STRING" '  # We run awk ONCE per file.
     BEGIN {
         split(headers, header_array, ",")  # Split the master header list into an awk array
-        
 
         header_count = length(header_array) # Get length of header array
     }
-    
+
     # Read every line of the file and store its key-value pair to memory
     {
         # Store value, key is lowercase parameter name, skip empty
         if ($1 != "") {
-  
-            value = $2             # Combine all fields after the first, in case value has a comma
+
+            value = $2            # Combine all fields after the first, in case value has a comma
             for (i=3; i<=NF; i++) {
                 value = value "," $i
             }
             data[tolower($1)] = value
         }
     }
-    
     # Runs once after the whole file is read
     END {
 
         for (i = 1; i <= header_count; i++) { # Loop function to go through the headers IN ORDER
             header = header_array[i]
-            
-            
+
             value = data[header]  # Get the value from our data array
-            
-           
-            
+
             printf "%s", value # Print the value. If it was empty, this prints nothing.
             if (i < header_count) {
                 printf "," # Add a comma, but not for the last item.
             }
         }
-       
+
         printf "\n"  # Print a final newline for the row
     }
     ' "$csv_file" >> "$output_file" # Awk reads the file and appends its output
@@ -102,5 +72,3 @@ done
 
 printf "\n"
 echo "Done! Reformatted files are in $OUTPUT_DIR"
-
-
