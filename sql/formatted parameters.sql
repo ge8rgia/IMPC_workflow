bcba2cf0b78623380caafd087a65d64fb0cb8c99
YJ <yanjing.zkg@gmail.com>
2025-11-13 21:24:21 +0000

    Update formatted parameters.sql

--- sql/formatted parameters.sql
+++ sql/formatted parameters.sql
@@ -1,15 +1,15 @@
-UPDATE cleaned_merged_output
+UPDATE Analysis
 SET parameter_name = 'Platelet count'
-WHERE pparameter_name = 'Platelets count';
+WHERE parameter_name = 'Platelets count';
 
-UPDATE cleaned_merged_output
+UPDATE Analysis
 SET parameter_name = 'Mean cell haemoglobin concentration'
 WHERE parameter_name = 'Mean cell hemoglobin concentration';
 
-UPDATE cleaned_merged_output
+UPDATE Analysis
 SET parameter_name = 'Mean cell volume'
 WHERE parameter_name = 'Mean-cell-volume';
 
-UPDATE cleaned_merged_output
+UPDATE Analysis
 SET parameter_name = 'Mean corpuscular haemoglobin'
 WHERE parameter_name IN ('Mean corpuscular hemoglobin', 'Mean-corpuscular-haemoglobin');
